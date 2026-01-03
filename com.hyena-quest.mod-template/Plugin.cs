cb207a08784c60c38a0ac04dd16e58b5521b0e2f
FailCake <4944278+edunad@users.noreply.github.com>
2026-01-03 13:49:05 +0100

    Initial commit

---
+++ com.hyena-quest.mod-template/Plugin.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using BepInEx;
+using BepInEx.Logging;
+using HyenaQuest;
+using UnityEngine.SceneManagement;
+
+namespace com.hyena_quest.mod_template
+{
+    [BepInPlugin("com.hyena-quest.mod-template", "MyPlugin", "0.0.0.1")]
+    public class Plugin : BaseUnityPlugin
+    {
+        internal static new ManualLogSource Logger;
+
+        private void Awake()
+        {
+            Logger = base.Logger;
+            SceneManager.sceneLoaded += OnSceneLoaded;
+        }
+
+        private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+        {
+            if (scene.name != "INGAME") return;
+            Logger.LogInfo($"Running as server? {NETController.Instance.IsServer}");
+        }
+        
+        private void OnDestroy()
+        {
+            SceneManager.sceneLoaded -= OnSceneLoaded;
+        }
+    }
+}
